bbb91cd02370e3882878a9e5c0f435c3610f248a
Sune Kaae <sune@kaae.name>
2014-04-21 12:47:23 +0200

    cleanup, variable names

--- irb_utils.rb
+++ irb_utils.rb
@@ -5,9 +5,9 @@ def gogo_selenium
 end
 
 def start_selenium
-  $browser = Selenium::WebDriver.for(:firefox)
-  $browser.get(get_local_url)
-  $browser.manage.window.resize_to(200, 200)
+  @browser = Selenium::WebDriver.for(:firefox)
+  @browser.get(get_local_url)
+  @browser.manage.window.resize_to(200, 200)
   puts 'browser instance available in $browser'
 end
 
@@ -16,30 +16,30 @@ def get_local_url
 end
 
 def get_clientWidth
-  $browser.execute_script("return document.documentElement.clientWidth")
+  @browser.execute_script("return document.documentElement.clientWidth")
 end
 
 def get_clientHeight
-  $browser.execute_script("return document.documentElement.clientHeight")
+  @browser.execute_script("return document.documentElement.clientHeight")
 end
 
 
 # useful highlight code for ruby *
 # https://gist.github.com/marciomazza/3086536
 def highlight(method, locator, ancestors=0)
-  $element = $browser.find_element(method, locator)
+  @element = $browser.find_element(method, locator)
   highlight_element($element)
 end
 
 def highlight_element(element, ancestors=0)
-  $browser.execute_script("hlt = function(c) { c.style.border='solid 2px rgb(255, 16, 16)'; }; return hlt(arguments[0]);", element)
+  @browser.execute_script("hlt = function(c) { c.style.border='solid 2px rgb(255, 16, 16)'; }; return hlt(arguments[0]);", element)
   parents = ""
   red = 255
 
   ancestors.times do
     parents << ".parentNode"
     red -= (12*8 / ancestors)
-    $browser.execute_script("hlt = function(c) { c#{parents}.style.border='solid 1px rgb(#{red}, 0, 0)'; }; return hlt(arguments[0]);", element)
+    @browser.execute_script("hlt = function(c) { c#{parents}.style.border='solid 1px rgb(#{red}, 0, 0)'; }; return hlt(arguments[0]);", element)
   end
 end
 
